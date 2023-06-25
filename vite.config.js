083ba9533d6abf5e6218f36305a75179c03041c3
Yicong Jia <yjia7@jaguarlandrover.com>
2023-06-25 13:59:28 +0800

    element主题色定制

--- vite.config.js
+++ vite.config.js
@@ -14,12 +14,27 @@ export default defineConfig({
       resolvers: [ElementPlusResolver()],
     }),
     Components({
-      resolvers: [ElementPlusResolver()],
+      // 1. 配置elementPlus采用sass样式配色系统
+      resolvers: [ElementPlusResolver({importStyle: "sass"})],
     }),
+    // 按需定制主题配置
+    // ElementPlus({
+    //   useSource: true,
+    // }),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // 自动导入定制化样式文件进行样式覆盖
+        additionalData: `
+          @use "@/styles/element/index.scss" as *;
+        `,
+      }
     }
   }
 })
