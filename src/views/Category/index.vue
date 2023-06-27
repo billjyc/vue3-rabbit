23f64c942b16889bc5fbab8237c18910e7f79905
Yicong Jia <yjia7@jaguarlandrover.com>
2023-06-27 16:36:55 +0800

    路由缓存问题解决

--- src/views/Category/index.vue
+++ src/views/Category/index.vue
@@ -2,17 +2,25 @@
 import {getCategoryAPI} from "@/apis/category";
 import {ref, onMounted} from "vue";
 import {getBannerAPI} from '@/apis/home'
-import {useRoute} from "vue-router";
+import {onBeforeRouteUpdate, useRoute} from "vue-router";
 import GoodsItem from "@/views/Home/components/GoodsItem.vue";
+
 
 const categoryData = ref({});
 const route = useRoute();
-const getCategory = async ()=> {
-  const res = await getCategoryAPI(route.params.id);
+const getCategory = async (id=route.params.id)=> {
+  const res = await getCategoryAPI(id);
   categoryData.value = res.result;
 }
 
 onMounted(() => getCategory());
+
+// 目标：路由参数变化的时候，可以把分类数据接口重新发送
+onBeforeRouteUpdate((to) => {
+  console.log(to);
+  getCategory(to.params.id);
+});
+
 
 //获取 banner
 const bannerList = ref([]);
