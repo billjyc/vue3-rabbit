4404c96f1d93d2b47cefb13acc29b1f2843b806d
Yicong Jia <yjia7@jaguarlandrover.com>
2023-06-27 15:09:04 +0800

    home-图片懒加载指令实现

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import {createApp} from 'vue'
+import {createPinia} from 'pinia'
 
 import App from '@/App.vue'
 import router from '@/router'
 import '@/styles/common.scss'
+import {useIntersectionObserver} from "@vueuse/core";
+
 
 const app = createApp(App)
 
@@ -11,3 +13,20 @@ app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
+
+app.directive('img-lazy', {
+    mounted(el, binding) {
+        //el: 指令绑定的元素
+        //binding: binding.value 指令等于后面绑定的表达式的值 图片 url
+        console.log(el, binding.value);
+        const {stop} = useIntersectionObserver(el,
+            ([{isIntersecting}]) => {
+                console.log(isIntersecting);
+                if (isIntersecting) {
+                    //进入视口区域
+                    el.src = binding.value;
+                    stop();
+                }
+            })
+    }
+})
