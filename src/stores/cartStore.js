a4556c76a189455f2060e9ba12798624607b037f
Yicong Jia <yjia7@jaguarlandrover.com>
2023-07-03 11:15:08 +0800

    本地购物车-列表购物车-统计计算

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -36,10 +36,16 @@ export const useCartStore = defineStore('cart', () => {
   const allCount = computed(()=>cartList.value.reduce((a, c) => a + c.count, 0));
   const allPrice = computed(() => cartList.value.reduce((a, c) => a + c.count * c.price, 0));
   const isAll = computed(() => cartList.value.every((item) => item.selected))
+  //已选择数量
+  const selectedCount = computed(() => cartList.value.filter(item => item.selected).reduce((a, c) => a + c.count, 0));
+  // 已选择商品价钱合计
+  const selectedPrice = computed(() => cartList.value.filter(item => item.selected).reduce((a, c) => a + c.count * c.price, 0));
   return {
     cartList,
     allCount,
     allPrice,
+    selectedCount,
+    selectedPrice,
     addCart,
     delCart,
     singleCheck,
